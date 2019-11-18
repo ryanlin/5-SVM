fc8e3ec8cdf5217adcc8196588b7a26ae76f2444
ryanlin <ruilinl@nevada.unr.edu>
2019-11-18 13:19:59 -0800

    implemented compute_A and compute_cov

--- pca.py
+++ pca.py
@@ -10,13 +10,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def compute_Z(X, centering=True, scaling=False):
-  return 0
+  Z = X
+  if centering:
+    Z = Z - np.mean(Z,axis=0)
+  if scaling:
+    Z = Z / np.std(Z, axis=0)
+  return Z
 
 def compute_covariance_matrix(Z):
-  return 0
+  return np.dot(Z.T, Z)   # (Z^T)(Z)
 
 def find_pcs(COV):
+  f = COV.shape[0]            # num features
+  eig_vals = np.array((1,f))  # eigen values
+  eig_vecs = np.array((f,f))  # eigen vectors
+
   return 0
 
 def project_data(Z, PCS, L, k, var):
   return 0
+
