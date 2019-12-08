c8d58086e98239dec8173f710c89b74a918a5219
ryanlin <ruilinl@nevada.unr.edu>
2019-12-07 21:09:52 -0800

    First commit

---
+++ binary_classification.py
@@ -0,0 +1,18 @@
+import helpers
+
+
+def svm_train_brute(training_data):
+  return 0 # w, b, S
+
+
+def distance_point_to_hyperplane(pt, w, b):
+  return 0 # dist
+
+
+def compute_margin(data, w, b):
+  return 0 # margin
+
+
+def svm_test_brute(w, b, x):
+  return 0 # y
+
